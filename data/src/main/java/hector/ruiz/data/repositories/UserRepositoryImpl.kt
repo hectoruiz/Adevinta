c441d6bde5103b863ccae8115760db6bee6ca786
hectoruiz <hectoruiz.89@gmail.com>
2022-10-19 23:21:10 +0200

    Create basic structure based on Clean Architecture Create file to centralize all libraries versions Update compile SDK, Kotlin, Gradle and Gradle Plugin

--- data/src/main/java/hector/ruiz/data/repositories/UserRepositoryImpl.kt
+++ data/src/main/java/hector/ruiz/data/repositories/UserRepositoryImpl.kt
@@ -1,11 +1,12 @@
 package hector.ruiz.data.repositories
 
-import hector.ruiz.domain.entities.User
+import hector.ruiz.commons.ResponseResult
+import hector.ruiz.domain.entities.UserResponse
 import hector.ruiz.domain.repositories.UserRepository
 
 class UserRepositoryImpl : UserRepository {
 
-    override fun getUsers(amount: Int): List<User> {
+    override fun getUsers(pageNumber: Int): ResponseResult<UserResponse> {
         TODO("Not yet implemented")
     }
 }
