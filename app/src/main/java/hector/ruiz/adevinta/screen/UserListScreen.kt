841d9f73939278b18f1caf2e786179ef4b019472
hectoruiz <hectoruiz.89@gmail.com>
2022-10-21 21:33:17 +0200

    Add missing changes

--- app/src/main/java/hector/ruiz/adevinta/screen/UserListScreen.kt
+++ app/src/main/java/hector/ruiz/adevinta/screen/UserListScreen.kt
@@ -1,42 +1,42 @@
 package hector.ruiz.adevinta.screen
 
-import android.net.Uri
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
 import hector.ruiz.adevinta.R
 import hector.ruiz.adevinta.displaySnackBarShort
 import hector.ruiz.adevinta.ui.composables.CircularProgress
 import hector.ruiz.adevinta.ui.composables.FABButton
-import hector.ruiz.adevinta.ui.composables.Item
+import hector.ruiz.adevinta.ui.composables.Searcher
+import hector.ruiz.adevinta.ui.composables.UserList
 import hector.ruiz.adevinta.ui.theme.AdevintaTheme
 import hector.ruiz.adevinta.viewmodels.UserViewModel
-import hector.ruiz.domain.entities.User
+import kotlinx.coroutines.delay
 
 @Composable
 fun UserListScreen(navController: NavHostController) {
 
     val userViewModel = hiltViewModel<UserViewModel>()
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+
+    LaunchedEffect(key1 = text) {
+        delay(1500L)
+        userViewModel.filterUsers(text.text)
+    }
 
     AdevintaTheme {
-        val snackbarHostState = remember { SnackbarHostState() }
-        val scope = rememberCoroutineScope()
-
         Scaffold(
             topBar = {
                 TopAppBar(title = { Text(text = stringResource(id = R.string.list_title)) })
@@ -47,27 +47,23 @@ fun UserListScreen(navController: NavHostController) {
                     .padding(paddingValues)
                     .fillMaxSize()
                     .padding(vertical = dimensionResource(id = R.dimen.small_padding_parent))) {
-                    val userList = userViewModel.uiListState.value.userList
-                    LazyColumn(
-                        content = {
-                            items(userList.size) { index ->
-                                Box(modifier =
-                                Modifier.clickable {
-                                    val moshi = Moshi.Builder().build()
-                                    val jsonAdapter: JsonAdapter<User> =
-                                        moshi.adapter(User::class.java)
-                                    val userJSON = Uri.encode(jsonAdapter.toJson(userList[index]))
-                                    navController.navigate("detaillist/$userJSON")
-                                }) {
-                                    Item(
-                                        userList[index],
-                                        painterResource(id = R.drawable.ic_delete),
-                                        stringResource(id = R.string.picture_description)) {
-                                        userViewModel.removeUser(userList[index])
-                                    }
-                                }
-                            }
+                    val userList = userViewModel.uiListState.value.filteredUsers
+                    Column {
+                        Searcher(text,
+                            { newText ->
+                                text = newText
+                            },
+                            {
+                                userViewModel.removeFilter()
+                                text = TextFieldValue("")
+                            })
+                        UserList(userList, { param ->
+                            navController.navigate("detaillist/$param")
+                        }, { user ->
+                            userViewModel.removeUser(user)
                         })
+                    }
+
                     if (userViewModel.uiListState.value.loading) {
                         CircularProgress(modifier = Modifier.align(Alignment.Center))
                     }
